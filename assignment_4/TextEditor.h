487080a1579bd6df93a0dc48ee66b876b9ba5501
Yelyzaveta Hladiuk <liza.gladuyk@gmail.com>
2024-06-27 22:42:44 +0300

    Created TextEditor header

--- assignment_4/TextEditor.h
+++ assignment_4/TextEditor.h
@@ -1,16 +1,20 @@
-//
-// Created by liz on 27.06.2024.
-//
-
 #ifndef TEXTEDITOR_H
 #define TEXTEDITOR_H
 
-
+#include "CaesarCipher.h"
+#include <string>
 
 class TextEditor {
+public:
+    TextEditor(const std::string& libPath);
 
+    void run();
+
+private:
+    CaesarCipher cipher;
+
+    void performEncrypt(const std::string& inputPath, const std::string& outputPath, int key);
+    void performDecrypt(const std::string& inputPath, const std::string& outputPath, int key);
 };
 
-
-
-#endif //TEXTEDITOR_H
+#endif // TEXTEDITOR_H
